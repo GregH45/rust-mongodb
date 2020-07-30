becc1f71fa23bb608c8c987ad101dcf58316bc27
Grégoire Harba <gregoire.harba@gmail.com>
2020-07-30 15:45:50 +0200

    add .env

--- src/lib/mongo.rs
+++ src/lib/mongo.rs
@@ -1,23 +1,28 @@
-use std;
-use std::env;
-
 use mongodb;
 use mongodb::Client;
 use mongodb::error::Result;
 use mongodb::options::{ClientOptions, StreamAddress};
 
+use dotenv;
+
 const DEFAULT_MONGO_ADDRESS: &'static str = "127.0.0.1";
+const DEFAULT_MONGO_PORT: u16 = 27017;
 
 pub fn establish_connection() -> Result<Client> {
-    let database_url = match env::var("MONGO_ADDRESS") {
+    let database_url = match dotenv::var("MONGO_ADDRESS") {
         Ok(value) => value,
         Err(_) => DEFAULT_MONGO_ADDRESS.to_string(),
+    };
+
+    let database_port = match dotenv::var("MONGO_PORT") {
+        Ok(value) => value.parse::<u16>().unwrap(),
+        Err(_) => DEFAULT_MONGO_PORT,
     };
 
     let options = ClientOptions::builder()
         .hosts(vec![StreamAddress {
             hostname: database_url,
-            port: Some(27017),
+            port: Some(database_port),
         }])
         .build();
 
